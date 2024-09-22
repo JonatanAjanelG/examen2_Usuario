341f92a8f06b6e85d392250703b4dd296a9fb57e
JonatanAjanelG <jonatanajanel2002gmail.com>
2024-09-22 02:38:23 -0600

    ningun cambio

--- app/routers/usuarios.router.js
+++ app/routers/usuarios.router.js
@@ -4,10 +4,10 @@ let router = express.Router();
 const Usuario = require('../controllers/usuarios.controller.js');
 
 // Routes for Usuario operations
-router.post('/api/usuario', Usuario.create); // Create a new user
-router.get('/api/usuarios', Usuario.retrieveAllUsuarios); // Get all users
-router.get('/api/usuario/:id', Usuario.getUsuarioById); // Get a user by ID
-router.delete('/api/usuarioD/:id', Usuario.deleteUsuarioById); // Delete a user by ID
-router.put('/api/usuarioA/:id', Usuario.updateUsuarioById); // Update a user by ID
+router.post('/api/usuario', Usuario.create); 
+router.get('/api/usuarios', Usuario.retrieveAllUsuarios);
+router.get('/api/usuario/:id', Usuario.getUsuarioById); 
+router.delete('/api/usuarioD/:id', Usuario.deleteUsuarioById); 
+router.put('/api/usuarioA/:id', Usuario.updateUsuarioById); 
 
 module.exports = router;
