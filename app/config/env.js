5592f6285b0080cde4d77e535c521a78a264f605
JonatanAjanelG <jonatanajanel2002gmail.com>
2024-10-30 03:48:35 -0600

    cambio de datos correctos nueva BD

--- app/config/env.js
+++ app/config/env.js
@@ -1,9 +1,9 @@
 const env = {
     
-    database: 'umg_salama_16431_wvn6',
+    database: 'umg_salama_16431_kth5',
     username: 'umg_salama_16431_kth5_user',
     password: 'bEWtsz8JL5LHdAD5jxXEIz6FiTAo5G8F',
-    //host: 'dpg-crmgum23esus73fsvheg-a.oregon-postgres.render.com', //este es el host externo
+    //host: 'dpg-csgvk588fa8c7394drh0-a.oregon-postgres.render.com', //este es el host externo
    host: 'dpg-csgvk588fa8c7394drh0-a', //usar este para subir el proyecto a render final
     dialect: 'postgres',
     
