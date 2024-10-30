5f7f50fbea267f50a67cfaa86ee249c83da5710f
JonatanAjanelG <jonatanajanel2002gmail.com>
2024-10-30 03:42:54 -0600

    cambio de datos nueva BD

--- app/config/env.js
+++ app/config/env.js
@@ -4,11 +4,7 @@ const env = {
     username: 'umg_salama_16431_kth5_user',
     password: 'bEWtsz8JL5LHdAD5jxXEIz6FiTAo5G8F',
     //host: 'dpg-crmgum23esus73fsvheg-a.oregon-postgres.render.com', //este es el host externo
-<<<<<<< HEAD
    host: 'dpg-csgvk588fa8c7394drh0-a', //usar este para subir el proyecto a render final
-=======
-    host: 'dpg-crmgum23esus73fsvheg-a', //usar este para subir el proyecto a render final
->>>>>>> e8dcfcc2245452f654ef15d0b6031a80f1c17966
     dialect: 'postgres',
     
     pool: {
